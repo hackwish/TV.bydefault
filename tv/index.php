bb530d0e27e7048ef3c545d29e34acc6f1a0f0b0
hackwish <marcelo.cardenasc@gmail.com>
2017-08-20 01:20:59 -0300

    update 200817

--- tv/index.php
+++ tv/index.php
@@ -7,7 +7,7 @@ echo "#EXTM3U \r\n";
 
 //CONTENIDO CANAL
     while($row = $result->fetch_assoc()) {
-        echo "#EXTINF:0, radio='" . $row['radio']. "' group-title='" . $row['descgrupo']. "' tvg-logo='" . $row['logocanal']. "', " . $row['nomcanal']. "\r\n" . $row['url']. "\r\n";
+        echo "#EXTINF:0, radio=\"" . $row['radio']. "\" group-title=\"" . $row['descgrupo']. "\" tvg-logo=\"" . $row['logocanal']. "\", " . $row['nomcanal']. "\r\n" . $row['url']. "\r\n";
         }
 
 $conn->close();
