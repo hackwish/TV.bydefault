1f1f465d74a2e5ef95c28b37e8e4f1195848fce0
hackwish <marcelo.cardenasc@gmail.com>
2017-08-20 01:24:29 -0300

    update 200817 fix lista

--- tv/index.php
+++ tv/index.php
@@ -2,13 +2,6 @@
 
 include 'config/db.php';
 
-// Cabecera Playlist
-echo "#EXTM3U \r\n";
+include 'config/list.php';
 
-//CONTENIDO CANAL
-    while($row = $result->fetch_assoc()) {
-        echo "#EXTINF:0, radio=\"" . $row['radio']. "\" group-title=\"" . $row['descgrupo']. "\" tvg-logo=\"" . $row['logocanal']. "\", " . $row['nomcanal']. "\r\n" . $row['url']. "\r\n";
-        }
-
-$conn->close();
 ?>
